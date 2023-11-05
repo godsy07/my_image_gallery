ec1a792155ddca8ec780bcbe1a6c1662577bbeca
unknown <godsonemmanuel07@gmail.com>
2023-11-05 11:53:55 +0530

    modified toast state and div to add animation effects

--- src/components/wrapper/ToastContainerDiv.jsx
+++ src/components/wrapper/ToastContainerDiv.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { useToast } from '../../context/ToastContext'
+import { AnimatePresence, motion } from 'framer-motion';
 import { Toast, ToastContainer } from 'react-bootstrap';
+import { useToast } from '../../context/ToastContext'
 
 const ToastContainerDiv = () => {
     const { toasts, removeToast } = useToast();
@@ -26,35 +27,44 @@ const ToastContainerDiv = () => {
         light: "rgb(255, 255, 255)",
         dark: "rgb(0, 0, 0)",
     }
+
+    const toastVariants = {
+        hidden: { opacity: 0, y: -50 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+        exit: { opacity: 0, x: -100 },
+      };
     
   return (
     <div className='w-100' style={{ position: "fixed", top: "0", right: "0", backgroundColor: "transparent", zIndex: "2000" }}>
       <ToastContainer position='top-end' className='mt-3'>
-        {toasts.map((toast) => (
-            <Toast
-                key={toast.id}
-                onClose={() => removeToast(toast.id)}
-            >
-                <Toast.Header
-                    className='d-flex justify-content-between align-items-center'
-                    closeVariant={`${['dark','primary','error'].includes(toast.type)?"white":"black"}`}
-                    style={{
-                        backgroundColor: toastHeaderBgColors[toast.type] || "white",
-                        color: ['dark','primary','error'].includes(toast.type)?"white":"black",
-                    }}
-                >
-                    <strong>{toast.heading}</strong>
-                </Toast.Header>
-                <Toast.Body
-                    style={{
-                        backgroundColor: toastBodyBgColors[toast.type] || "white",
-                        color: ['dark','primary','error'].includes(toast.type)?"white":"black",
-                    }}
-                >
-                    {toast.message}
-                </Toast.Body>
-            </Toast>
-        ))}
+          <AnimatePresence>
+            {toasts.map((toast) => (
+                <motion.div key={toast.id} variants={toastVariants} initial='hidden' animate='visible' exit='exit'>
+                    <Toast
+                        onClose={() => removeToast(toast.id)}
+                        className='m-0 mb-2'
+                    >
+                        <Toast.Header
+                            className='d-flex justify-content-between align-items-center'
+                            style={{
+                                backgroundColor: toastHeaderBgColors[toast.type] || "white",
+                                color: ['dark','primary','error'].includes(toast.type)?"white":"black",
+                            }}
+                        >
+                            <strong>{toast.heading?toast.heading:toast.type}</strong>
+                        </Toast.Header>
+                        <Toast.Body
+                            style={{
+                                backgroundColor: toastBodyBgColors[toast.type] || "white",
+                                color: ['dark','primary','error'].includes(toast.type)?"white":"black",
+                            }}
+                        >
+                            {toast.message}
+                        </Toast.Body>
+                    </Toast>
+                </motion.div>
+            ))}
+        </AnimatePresence>
       </ToastContainer>
     </div>
   )
--- src/context/ToastContext.jsx
+++ src/context/ToastContext.jsx
@@ -1,31 +1,36 @@
 // ToastContext.js
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
 
 const ToastContext = createContext();
 
 export const ToastProvider = ({ children }) => {
   const [toasts, setToasts] = useState([]);
-  // const [toasts, setToasts] = useState([
-  //   { id: 1, message: "This is test message 1", type: 'success', heading: "Success" },
-  //   { id: 2, message: "This is test message 2", type: 'error', heading: "Error" },
-  //   { id: 3, message: "This is test message 2", type: 'warning', heading: "Warning" },
-  //   { id: 4, message: "This is test message 2", type: 'info', heading: "Info" },
-  //   { id: 5, message: "This is test message 2", type: 'primary', heading: "Primary" },
-  //   { id: 6, message: "This is test message 2", type: 'secondary', heading: "Secondary" },
-  //   { id: 7, message: "This is test message 2", type: 'light', heading: "Light" },
-  //   { id: 8, message: "This is test message 2", type: 'dark', heading: "Dark" },
-  // ]);
 
-  const addToast = ({ message, type, delay=3000 }) => {
-    const newToast = { id: Date.now(), message, type };
-    setToasts((prevToasts) => [...prevToasts, newToast]);
-
-    setTimeout(() => removeToast(addToast.id), delay);
+  const addToast = ({ message, heading, type, duration=3000 }) => {
+    const newToast = { id: Date.now(), heading, message, type, duration, createdTime: Date.now() };
+    setToasts((prevToasts) => [ newToast, ...prevToasts ]);
   };
 
   const removeToast = (id) => {
     setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
   };
+
+  useEffect(() => {
+    let toastCleanupInterval;
+    if (toasts.length > 0) {
+      // Periodically check and remove old toasts
+      toastCleanupInterval = setInterval(() => {
+        const currentTime = Date.now();
+        const updatedToasts = toasts.filter(
+          (toast) => currentTime - toast.createdTime <= toast.duration
+        );
+        setToasts(updatedToasts);
+      }, 10); // Check every 10 microsecond
+
+    }
+
+    return () => clearInterval(toastCleanupInterval);
+  }, [toasts]);
 
   return (
     <ToastContext.Provider value={{ toasts, addToast, removeToast }}>
